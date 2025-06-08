cb20f0cf8b0491051c80354cf30e21be437af96f
Raghav Verma <raghav2103rv@gmail.com>
2025-06-08 20:15:06 +0530

    chapter 2 completed

---
+++ chapter 2/main.js
@@ -0,0 +1,109 @@
+console.log("----Hello Operators🔥----")
+
+// *1. Arithmetic Operators (+ , - , * , %(remainder) , ** (exponential) , ++ , --)
+
+let a = 20;
+let b = 10; 
+
+// console.log(++a) //21
+// console.log(a++) //21
+// console.log(a) //22
+// console.log(a--) //22
+// console.log(--a) //20
+// console.log(a) //20
+// a++;
+
+// console.log(++a) //21
+// console.log(a++) // 20
+// console.log(a); //21
+
+// b++;
+// console.log(b)
+// console.log(a)
+
+// a--;
+// b--;
+// console.log(a)
+// console.log(b)
+
+// console.log(a+b) // 30
+// console.log(a-b) // 10
+// console.log(a*b) //200
+// console.log(a%b) //0
+// console.log(a**b)
+
+
+
+// *2. Assignment Operators (= , += , -= , *= , **=  , /= , %=)
+
+// let a = 44;
+
+// let a = 10;
+
+// console.log(a %=2)
+
+// console.log(a/=2)
+// console.log(a**=2)
+// console.log(a*=3)
+// console.log(a-=2)
+// console.log(a+=5)
+
+
+// // a += 5
+// // a -= 40;
+// a **= 2;
+// console.log(a)
+
+// *3. Comparison Operators
+
+
+// let num1 = 10;
+// let num2 = 100;
+
+// console.log(num1 >= num2)
+// console.log(num1 <= num2)
+
+// console.log(num1 < num2)
+
+// console.log(num1 != num2) //value
+
+// console.log(num1 !== num2)
+
+
+// console.log(num1 == num2) //values compares
+
+// console.log(num1 === num2) //value + datatypes
+
+
+// *4. Logical Operators (&& (AND) , || (OR) , !(NOT))
+
+// !. (&& and) l.h.s , r.h.s = dono true honge tabhi mai true return krunga 
+
+console.log(true && true)
+console.log(false && true)
+console.log(true && false)
+console.log(false && false)
+
+// * TRUE && TRUE = TRUE
+// * FALSE && TRUE = FALSE
+// * TRUE && FALSE = FALSE
+// * FALSE && FALSE = FALSE;
+
+
+// !. (|| or) = kahi bhi true hoga toh mai true return krdunga
+
+console.log(true || true)
+console.log(false || true)
+console.log(true || false)
+console.log(false || false)
+// * TRUE && TRUE = TRUE
+// * FALSE && TRUE = TRUE
+// * TRUE && FALSE = TRUE
+// * FALSE && FALSE = FALSE;
+
+
+// ?. (! (not))
+console.log(!true)
+console.log(!false)
+    // !TRUE = false;
+    // !FALSE = TRUE;
