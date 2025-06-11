854ce18d5095c27823a3af105766dbd67609d5bf
Raghav Verma <90718322+rv-raghav@users.noreply.github.com>
2025-06-11 07:44:45 +0000

    chapter 5 completed

---
+++ Basics/chapter 5/main.js
@@ -0,0 +1,54 @@
+// // function sayHello(){
+// //     console.log("Hello world!")
+// // }
+
+// // sayHello()
+// // sayHello()
+
+// // *function decleration✅
+// // *function reference✅
+// // *function calling✅
+
+
+// function add(number1 , number2 ){
+//    return number1 + number2
+// }
+
+// let res = add(30 , 50)
+
+// console.log(res)
+// function mul(number1 , number2){
+//    return number1 * number2
+// }
+
+
+// mul(2,2)
+
+// // *Parameters ==> function decleration 
+// // *Arguments ==> function calling ke waqt
+
+// // console.log(40 + 20)
+
+// Anonymous 
+
+// const getUsername = function(username="Akash"){
+//     return "Hello" + username
+// }
+
+// const res = getUsername()
+
+// console.log(res)
+
+
+// const ConsoleFruits = fruit=>console.log(fruit)
+
+
+// ConsoleFruits("🍎")
+
+// IIFE
+
+(
+    function(){
+        console.log("IIFE")
+    }
+)()
